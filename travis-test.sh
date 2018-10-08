c1c1eeed11e08c2d9cd8e8f3d1e819077fcbc204
Bryan Phelps <bryphe@outlook.com>
2018-10-07 19:41:59 -0700

    Flip back to using daemon, but in esy context

--- travis-test.sh
+++ travis-test.sh
@@ -9,13 +9,11 @@ set -e
   export DISPLAY
   LIBGL_ALWAYS_SOFTWARE=1
   export LIBGL_ALWAYS_SOFTWARE
-  sh -e /etc/init.d/xvfb start
+  /sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x32 +extension GLX +render
   sleep 5
 
   glxinfo
 # fi
 
-# https://stackoverflow.com/questions/28837117/travis-ci-run-lwjgl-tests-in-non-headless-environment
-# - /sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -screen 0 1400x900x24 -ac +extension GLX +render
 
 dune runtest
