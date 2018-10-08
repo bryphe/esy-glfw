bd960de85c79e4c120df7c3e38976e97780eedab
Bryan Phelps <bryphe@outlook.com>
2018-10-07 18:27:28 -0700

    Try installing xfvb just for linux

---
+++ travis-test.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+echo Travis build - detected OS is: "$TRAVIS_OS_NAME"
+set -e
+
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+  # Initialize display driver
+  DISPLAY=:99.0
+  export DISPLAY
+  sh -e /etc/init.d/xvfb start
+  sleep 3
+fi
+
+# https://stackoverflow.com/questions/28837117/travis-ci-run-lwjgl-tests-in-non-headless-environment
+# - /sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -screen 0 1400x900x24 -ac +extension GLX +render
+
+esy b dune runtest
