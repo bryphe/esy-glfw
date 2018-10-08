8274579ab82fde11f7eb1f235ff9270940c666cb
Bryan Phelps <bryphe@outlook.com>
2018-10-07 18:29:52 -0700

    Fix lib

--- travis-test.sh
+++ travis-test.sh
@@ -7,8 +7,11 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
   # Initialize display driver
   DISPLAY=:99.0
   export DISPLAY
+  LIBGL_ALWAYS_SOFTWARE=1
+  export LIBGL_ALWAYS_SOFTWARE
   sh -e /etc/init.d/xvfb start
-  sleep 3
+  sleep 5
+  glxinfo
 fi
 
 # https://stackoverflow.com/questions/28837117/travis-ci-run-lwjgl-tests-in-non-headless-environment
